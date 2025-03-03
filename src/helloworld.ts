beaf77f6a010a41b3da4d52178f81f5385b65f99
Vitaly Belenky <v.belenkij@sigma-it.ru>
2025-03-03 08:51:26 +0300

    first commit

---
+++ src/helloworld.ts
@@ -0,0 +1,10 @@
+// 
+// https://www.surfsidemedia.in/post/creating-a-simple-web-application-with-typescript
+// 
+// https://dev.to/mariecolvinn/introduction-to-typescript-build-your-first-simple-web-application-1j5j
+// 
+// https://khalilstemmler.com/blogs/typescript/node-starter-project/
+// https://github.com/stemmlerjs/simple-typescript-starter
+//
+let message: string = 'Hello World';
+console.log(message);
