27ac6fdb5b111473deb231f912fb3c987dbc6b41
Vitaly Belenky <v.belenkij@sigma-it.ru>
2025-03-06 18:21:47 +0300

    Add testing with jest

--- src/cronparser.module.test.ts
+++ src/cronparser.module.test.ts
@@ -1,30 +1,30 @@
 import {CronParser, CronResult, EveryTick} from "./cronparser.module";
 
+// * * * * * <command to execute>
+// | | | | |
+// | | | | day of the week (0–6) (Sunday to Saturday;
+// | | | month (1–12)             7 is also Sunday on some systems)
+// | | day of the month (1–31)
+// | hour (0–23)
+// minute (0–59)
+
+// * * * * *
+// ? * * * *
+// */5 * * * *
+// * */5 * * *
+// * * 3-4 * *
+// * * * JAN-DEC *
+// * * * * 0-6
 test("Every second", () => {
     // expect(fizzBuzz(2)).toBe("1 2 ");
     // expect(fizzBuzz(3)).toBe("1 2 Fizz ");
 
-    let line: string = "* * * * *";
-    line = line.trim();
-    let zeroLength = line.length - 1;
-    let pos = 0;
-    let result = [];
-    let current = null;
-    for (let i = 0; i < line.length; i++) {
-        const ch: string = line[i];
-        // console.log(char);
-        if (ch === "*") {
-            current = new EveryTick();
-        } else if (ch === " ") {
-            result[pos] = current;
-            pos++;
-        } else {
-            throw new Error('Unexpected character "' + ch + '"');
-        }
-    }
-    // last
-    result[pos] = current;
+    let cronResult = new CronParser().parse("* * * * *");
+    let everyTick = new EveryTick();
 
-    new CronResult(result);
-
+    expect(cronResult.minute.isEqual(everyTick)).toBe(true);
+    expect(cronResult.hour.isEqual(everyTick)).toBe(true);
+    expect(cronResult.dayOfMonth.isEqual(everyTick)).toBe(true);
+    expect(cronResult.month.isEqual(everyTick)).toBe(true);
+    expect(cronResult.dayOfWeek.isEqual(everyTick)).toBe(true);
 });
--- src/cronparser.module.ts
+++ src/cronparser.module.ts
@@ -1,28 +1,33 @@
 export class CronParser {
-    // public name: string;
-    // public color: string;
-    //
-    // constructor(name: string, color: string) {
-    //     this.name = name;
-    //     this.color = color;
-    // }
-    //
-    // public move(distanceMeter: number) : string {
-    //     return `${this.name} moved ${distanceMeter}m.`;
-    // }
-    //
-    // public say() : string {
-    //     return `Cat ${this.name} says meow`;
-    // }
+    public parse(line: string): CronResult {
+        //let line: string = "* * * * *";
+        line = line.trim();
+        //let zeroLength = line.length - 1;
+        let pos = 0;
+        let result: Array<EveryTick> = [];
+        let current = new EveryTick();
+        for (let i = 0; i < line.length; i++) {
+            const ch: string = line[i];
+            if (ch === "*") {
+                current = new EveryTick();
+            } else if (ch === " ") {
+                result[pos] = current;
+                pos++;
+            } else {
+                throw new Error('Unexpected character "' + ch + '"');
+            }
+        }
+        // last
+        result[pos] = current;
 
-    public parse(cronline: string) : string | null {
-        //return `Cat ${this.name} says meow`;
-        return null;
+        return new CronResult(result);
     }
 }
 
 export class EveryTick {
-
+    isEqual(other: EveryTick): boolean {
+        return this instanceof EveryTick && other instanceof EveryTick;
+    }
 }
 
 // * * * * * <command to execute>
@@ -39,11 +44,20 @@ export class CronResult {
     month: EveryTick;
     dayOfWeek: EveryTick;
 
-    constructor(result: any[]) {
-        this.minute = [0];
-        this.hour = result[1];
-        this.dayOfMonth = result[2];
-        this.month = result[3];
-        this.dayOfWeek = result[4];
+    // constructor(result: any[]) {
+    //     this.minute = result[0];
+    //     this.hour = result[1];
+    //     this.dayOfMonth = result[2];
+    //     this.month = result[3];
+    //     this.dayOfWeek = result[4];
+    // }
+
+    constructor([minute, hour, dayOfMonth, month, dayOfWeek]: EveryTick[]) {
+        this.minute = minute;
+        this.hour = hour;
+        this.dayOfMonth = dayOfMonth;
+        this.month = month;
+        this.dayOfWeek = dayOfWeek;
     }
+
 }
